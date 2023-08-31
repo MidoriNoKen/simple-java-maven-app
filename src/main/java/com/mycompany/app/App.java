12d95a3b818e3476dc16fb7543fadce6b8fe1907
MidoriNoKen <midor1nok3n@gmail.com>
2023-09-01 01:17:15 +0700

    go back

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,28 +1,20 @@
 package com.mycompany.app;
 
-import spark.*;
-
+/**
+ * Hello world!
+ */
 public class App {
+
     private static final String MESSAGE = "Hello World!";
 
     public App() {
     }
 
     public static void main(String[] args) {
-        Spark.port(getHerokuAssignedPort()); // Set port based on Heroku environment
-        get("/", (req, res) -> MESSAGE);
+        System.out.println(MESSAGE);
     }
 
     public String getMessage() {
         return MESSAGE;
-    }
-
-    // Get assigned port from Heroku environment
-    private static int getHerokuAssignedPort() {
-        ProcessBuilder processBuilder = new ProcessBuilder();
-        if (processBuilder.environment().get("PORT") != null) {
-            return Integer.parseInt(processBuilder.environment().get("PORT"));
-        }
-        return 4567; // Default port if not found
     }
 }
